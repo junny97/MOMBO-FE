7d0c9aa41aa75c46d0728e376b5b08ebf2c44b22
junny97 <pcohad12@gmail.com>
2024-10-10 08:01:06 +0000

    style: 버튼 공통 컴포넌트 디자인 완료

---
+++ src/components/common/button/largeButton.tsx
@@ -0,0 +1,71 @@
+import { ButtonHTMLAttributes } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+type ButtonVariant = 'outline' | 'fill';
+type ButtonColor = 'primary' | 'primary-dark' | 'secondary';
+
+// 유효한 조합만 허용하는 타입
+// ex) variant:outline, buttonColor:'primary' 조합 사용시x
+// variant: fills, buttonColor: 'primary' O
+type ButtonProps<V extends ButtonVariant> = V extends 'fill'
+  ? {
+      variant: V;
+      buttonColor: ButtonColor;
+    } & ButtonHTMLAttributes<HTMLButtonElement>
+  : V extends 'outline'
+    ? {
+        variant: V;
+        buttonColor?: never;
+      } & ButtonHTMLAttributes<HTMLButtonElement>
+    : never;
+
+const getVariant = (
+  variant: ButtonVariant,
+  color: ButtonColor,
+  disabled?: boolean,
+) => {
+  if (disabled) {
+    return 'bg-neutral-400 text-white';
+  }
+  if (variant === 'outline') {
+    return 'border-2 border-primary text-primary bg-white';
+  }
+  if (variant === 'fill') {
+    switch (color) {
+      case 'primary':
+        return 'bg-primary text-white';
+      case 'primary-dark':
+        return 'bg-primary-dark text-white';
+      default:
+        return 'bg-primary text-white';
+    }
+  }
+  return '';
+};
+
+export default function LargeButton<V extends ButtonVariant>({
+  children,
+  variant,
+  type = 'button',
+  className,
+  buttonColor,
+  disabled,
+  ...props
+}: ButtonProps<V>) {
+  const buttonClasses = twMerge(
+    'w-358 h-56 rounded-12 font-semibold',
+    getVariant(variant, buttonColor as ButtonColor, disabled),
+    className,
+  );
+
+  return (
+    <button
+      type={type}
+      disabled={disabled}
+      className={buttonClasses}
+      {...props}
+    >
+      <p>{children}</p>
+    </button>
+  );
+}

ef9794babb6e58a55fa4c469d7bfcd81f7f035fb
junny97 <pcohad12@gmail.com>
2024-10-10 07:14:04 +0000

    chore: tailwindcss-preset-px-to-rem 패키지 설치 & tailwindcss config 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
+  presets: [require('tailwindcss-preset-px-to-rem')],
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {

b52a284a81e8d99dbe5af0618464f34f915365fb
junny97 <pcohad12@gmail.com>
2024-10-11 06:59:44 +0000

    Fix: useNotificationPermission Reference Error 수정 - Notification 객체를 서버에서 사용할 수 없어 발생한 문제 -  클라이언트 측에서만 동작하도록 변경

--- src/hooks/notification/useNotificationPermission.ts
+++ src/hooks/notification/useNotificationPermission.ts
@@ -1,19 +1,28 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
 export const useNotificationPermission = () => {
-  const [permission, setPermission] = useState(Notification.permission);
+  const [permission, setPermission] = useState(() => {
+    if (typeof window !== 'undefined' && 'Notification' in window) {
+      return Notification.permission;
+    }
+    return 'default';
+  });
+
+  useEffect(() => {
+    if (typeof window === 'undefined' || !('Notification' in window)) {
+      console.warn('This browser does not support notifications.');
+    }
+  }, []);
 
   const requestPermission = useCallback(async () => {
-    if (!('Notification' in window)) {
-      console.warn('This browser does not support notifications.');
-      return;
-    }
-    if (permission !== 'granted') {
-      const newPermission = await Notification.requestPermission();
-      console.log('Notification permission:', newPermission);
-      setPermission(newPermission);
+    if (typeof window !== 'undefined' && 'Notification' in window) {
+      if (permission !== 'granted') {
+        const newPermission = await Notification.requestPermission();
+        console.log('Notification permission:', newPermission);
+        setPermission(newPermission);
+      }
     }
   }, [permission]);
 
-  return { requestPermission };
+  return { permission, requestPermission };
 };

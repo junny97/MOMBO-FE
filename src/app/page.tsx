427607b1df476891a786a2e86729778781a1e817
junny97 <pcohad12@gmail.com>
2024-10-16 08:31:03 +0000

    Refactor: 첫 랜딩 페이지 카카오 로그인 페이지로 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,74 +1,9 @@
-'use client';
-import { useToggle } from '../hooks/useToggle';
-import Input from '<prefix>/components/common/input';
-import NavBar from '<prefix>/components/common/bar/navbar/navBar';
-import BackTopBar from '<prefix>/components/common/bar/backTopBar';
-import MainTopBar from '<prefix>/components/common/bar/mainTopBar';
-import LargeButton from '<prefix>/components/common/button/largeButton';
-import MiddleButton from '<prefix>/components/common/button/middleButton';
-import Modal from '<prefix>/components/common/modal';
-import CheckButton from '<prefix>/components/common/button/checkButton';
-import { useState } from 'react';
-import { useNotificationPermission } from '<prefix>/hooks/notification/useNotificationPermission';
-import { useForegroundNotification } from '<prefix>/hooks/notification/useForegroundNotification';
+import LoginPage from './(auth)/login/page';
 
 export default function Home() {
-  const { requestPermission } = useNotificationPermission();
-  useForegroundNotification();
-  const [isSelected, toggleButton] = useToggle();
-  const [value, setValue] = useState('');
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.currentTarget.value;
-    const sanitizedValue = inputValue.replace(/\s/g, '');
-    if (sanitizedValue.length <= 8) {
-      setValue(sanitizedValue);
-    }
-  };
   return (
-    <div className='grid h-full w-full place-items-center gap-8'>
-      <LargeButton variant='fill' buttonColor='primary'>
-        primary
-      </LargeButton>
-      <LargeButton
-        variant='fill'
-        buttonColor='primary-dark'
-        onClick={requestPermission}
-      >
-        primary-dark
-      </LargeButton>
-      <LargeButton variant='outline'>outline</LargeButton>
-      <MiddleButton variant='fill' buttonColor='primary'>
-        primary
-      </MiddleButton>
-      <MiddleButton variant='fill' buttonColor='primary-dark'>
-        primary-dark
-      </MiddleButton>
-      <MiddleButton variant='outline'>outline</MiddleButton>
-      <Modal />
-      <MainTopBar />
-      <BackTopBar title='Top App Bar' />
-      <Input
-        maxLength={8}
-        onChange={handleInputChange}
-        placeholder='placeholder'
-      />
-      <Input
-        value={value}
-        maxLength={8}
-        placeholder='placeholder'
-        errorMessage='error message'
-        onChange={handleInputChange}
-      />
-      <Input
-        value={value}
-        maxLength={8}
-        placeholder='placeholder'
-        enableMessage='enable message'
-        onChange={handleInputChange}
-      />
-      <CheckButton isSelected={isSelected} onClick={toggleButton} />
-      <NavBar />
+    <div className='h-full w-full'>
+      <LoginPage />
     </div>
   );
 }
